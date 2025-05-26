fcf812ad0f28722a7464b3ee521bd267aa28c24f
sameer <sameerpaul1212@gmail.com>
2025-05-26 23:31:43 +0530

    JSX fundatmenets

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-// React Element (DOM element) => object => HTML
+//jsx // React Element
 
-// const heading = React.createElement("h1", {id:"heading1"}, "Im here") // React element not html element, creates an object
-// console.log(heading);
+const heading1 = <span>Hello World  </span>
 
-
-//jsx
 const heading = (<h1 className="head" tabIndex="1">
-    Hello World
+    {heading1}
+    Hello India
 </h1>); // this is not html inside js, this is jsx // creating react element using jsx
+
+// React functional Componenets
+const TitleComponent = () => {
+    return <h1>React Functional Title</h1>;
+}
+
+function handleClick() {
+  alert("Clicked!");
+}
+
+const number = 12;
+
+// const data = api.getData();
+
+const HeadingComponent2 = () => (
+
+    <div className="container">
+        <h2>{number} "-" {100 + 200}</h2>
+        {heading}
+        {/* {data} -> jsx will prevent cross site scripting if any api is sending any melicious data*/}
+        <TitleComponent />
+        {TitleComponent()}
+        <TitleComponent></TitleComponent>
+        <h1 className="head">React Functional Componenets</h1>
+        <button onClick={handleClick}>Click Me</button>
+    </div>
+);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 // root.render(heading)
-root.render(heading)
+root.render(<HeadingComponent2 />)
 
 
 
