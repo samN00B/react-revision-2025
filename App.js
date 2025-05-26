993c9102a72d633f0c9091aa7830ff5bd7a42f08
sameer <sameerpaul1212@gmail.com>
2025-05-26 20:29:46 +0530

    jsx start 2

--- App.js
+++ App.js
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom/client';
 
 // React Element (DOM element) => object => HTML
 
-const heading = React.createElement("h1", {id:"heading1"}, "Im here") // React element not html element, creates an object
-console.log(heading);
+// const heading = React.createElement("h1", {id:"heading1"}, "Im here") // React element not html element, creates an object
+// console.log(heading);
 
 
 //jsx
-const jsxHeading = <h1 id="heading1">Hello World</h1>; // this is not html inside js, this is jsx // creating react element using jsx
-console.log(jsxHeading);
+const heading = (<h1 className="head" tabIndex="1">
+    Hello World
+</h1>); // this is not html inside js, this is jsx // creating react element using jsx
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 // root.render(heading)
-root.render(jsxHeading)
+root.render(heading)
 
 
 
